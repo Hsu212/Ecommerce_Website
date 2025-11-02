21dee693740610e4e52b5c92b275c157fed5d0c1
HMWM <146718691+Hsu212@users.noreply.github.com>
2025-11-02 17:08:15 +0700

    Add files via upload

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+// src/pages/Profile.tsx
+import React from 'react';
+import AuthContainer from '../components/auth/AuthContainer';
+import '../styles/Auth.css';
+
+const Profile: React.FC = () => {
+  return <AuthContainer />;
+};
+
+export default Profile;
